63176543fa60f9f4e243402862e78e6652a90a80
zhoubing <zbzhoubing123@163.com>
2019-08-17 16:57:58 +0800

    Initial commit

---
+++ src/main/java/com/pachiraframework/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.pachiraframework.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zb
+ * @date 2019/8/17 0017 16:46
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
